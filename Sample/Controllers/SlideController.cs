3490c1f320526ae001678d252a997ca1732135bd
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-04 15:50:16 +1000

    Modified template, added slides, completed intro

---
+++ Sample/Controllers/SlideController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Controllers
+{
+    public class SlideController : Controller
+    {
+        public ActionResult Index(string index)
+        {
+            return View(index);
+        }
+    }
+}
