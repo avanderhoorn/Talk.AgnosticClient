5440040108beb6c554fd3458aeadd518234588ce
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-10 11:51:22 +1000

    Setup dynamic content

--- Sample/Scripts/jquery.slide.js
+++ Sample/Scripts/jquery.slide.js
@@ -14,11 +14,28 @@
             sibling = type === 1 ? selected.nextAll('article:first') : selected.prevAll('article:first');
                 
         if (sibling.length == 1) {
+            //Hide/show what we need
             selected.hide().removeClass('selected');
             sibling.show().addClass('selected');
-                    
+            
+            //Manage content
+            $('.content .selected').removeClass('selected');
+            $('.content li:eq(' + sibling.index('article') + ')').addClass('selected');
+
+            //Hold back click event
             if (event)
                 event.preventDefault();
         }
     };
+    
+    //Dynamically setup content
+    var contentHtml = '<h3>Content</h3><ul class="content">';
+    $('article h5:first-child').each(function () {
+        contentHtml += '<li>' + $(this).text() + '</li>';
+    });
+    contentHtml += '</ul>';
+    $('aside').append(contentHtml);
+    
+    $('.content li:first').addClass('selected');
+
 });
